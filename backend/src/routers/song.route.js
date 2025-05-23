a8da3654f7f0aabe7add2cc9a96dd819ecdd9b39
kami <vuvansu390@gmail.com>
2025-05-23 16:37:30 +0700

    test

--- backend/src/routers/song.route.js
+++ backend/src/routers/song.route.js
@@ -12,7 +12,7 @@ import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/", protectRoute, getAllSongs);
 router.get("/featured", getFeaturedSongs);
 router.get("/made-for-you", getMadeForYouSongs);
 router.get("/trending", getTrendingSongs);
