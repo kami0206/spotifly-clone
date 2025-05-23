335c454100f688446cbc8b97cd451b9234bd1a71
kami <vuvansu390@gmail.com>
2025-05-23 16:24:12 +0700

    server

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,20 +46,20 @@ app.use(
   })
 ); //
 
-// const tempDir = path.join(process.cwd(), "tmp");
-// cron.schedule("0 * * * *", () => {
-//   if (fs.existsSync(tempDir)) {
-//     fs.readdir(tempDir, (err, files) => {
-//       if (err) {
-//         console.log("error", err);
-//         return;
-//       }
-//       for (const file of files) {
-//         fs.unlink(path.join(tempDir, file), (err) => {});
-//       }
-//     });
-//   }
-// });
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
